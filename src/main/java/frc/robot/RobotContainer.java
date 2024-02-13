759c15f2df710e4f6c4197785819d97e44b819d3
Programming <programming@saintsrobotics.com>
2024-02-12 16:05:34 -0800

    Docs: Climber

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,14 +4,12 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.climberSubSystem;
 
 public class RobotContainer {
   private final XboxController m_controller = new XboxController(0);
@@ -25,7 +23,8 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-
+    //When Y button is pressed, the climber will toggle up or down
+    //based on current position, it will be reversed
     new JoystickButton(m_controller, Button.kY.value)
     .onTrue(new InstantCommand(climberSubSystem::toggle));
 
